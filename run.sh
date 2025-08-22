ca8c5ace65591390ae786d8282242d7f76ecb788
Munana122 <m.munana@alustudent.com>
2025-08-22 13:33:28 +0200

    Update and rename .gitignore to run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Move to the directory where you can run SparseMAtrix.cpp
+cd dsa/code || { echo "Directory not found!"; exit 1; }
+
+# Compile the C++ file (replace 'program.cpp' with your file name)
+g++ SparseMatrix.cpp -o sparseM
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful. Running program..."
+    ./program_exe
+else
+    echo "Compilation failed."
+fi
+
+
